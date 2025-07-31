d3ded1aa561d44a52ae90bb30f8d0da0de17c8ff
科技酱Official <lelednkex@163.com>
2025-07-31 18:46:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const articles = {
+        '1': { 
+            title: '欢迎', 
+            content: `
+                <h2>欢迎大家来到科技酱的技术文档✨🎉🎆</h2>
+                <img src="./img1.jpg" alt="科技酱" width="200"><img src="./img2.png" alt="电脑科学" width="200">
+                <p>在这里分享技术，资源，以及一些有趣的东西。</p>
+                <a href="https://space.bilibili.com/1768832152?spm_id_from=333.1007.0.0" target="_blank">Bilibili</a>
+                <p> <a href="https://www.rainyun.com/NjcxNjE1_" target="_blank">雨云</a>  优惠码: NjcxNjE1</p>
+                
+                <h3>最新文章</h3>
+                <ul>
+                    <li><a href="#2">国内如何访问Github</a></li>
+                </ul>
+
+                <br><br><br>
+
+                <h2>喝杯奶茶</h2>
+                <img src="./wechat.jpg" alt="WeChat" width="298"> <img src="./alipay.png" alt="AliPay" width="280">
+
+                <p>助力捐赠我们收购(dk.com)域名和更多域名以及服务器费用，我们将记录捐赠的名录，如成功通过大家的捐赠将(dk.com)域名收购，我们将会把域名作为邮箱等等服务供大家使用，虽然预计此域名收购可能需要几千万人民币但是我们相信总有一天可以成功，感谢大家的支持！，大家的一点一滴都有着大作用，我们会继续努力，创造更好的互联网，给IT行业做更多贡献</p>    
+                
+                <h3>打赏记录</h3>
+                <p>感谢各位的支持👋：</p>
+                <div class="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>昵称</th>
+                                <th>日期</th>
+                                <th>金额</th>
+                                <th>留言</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                            </tr>
+                             <tr>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                            </tr>
+                            <tr>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                                <td>...</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+                <br><br>
+            ` },
+
+
+
+        '2': { title: '国内如何访问Github', content: `<h2>国内如何访问Github</h2><p>在日常生活中，我们在Github上下载开源项目的时候总会无法访问，这期教程一定能解决你的烦恼✨</p> <br> <p>教程亲测可用</p> <h3>Watt Toolkit(小白推荐)</h3> <p>Watt Toolkit是一个免费的Steam,Github等等外网加速工具，可以很好的解决无法访问Github的问题，不仅能加速Github还能加速Steam，微软云盘等等。<img src="https://steampp.net/images/home/js.webp" alt="Watt Toolkit" width="500">  官网链接: <a href="https://steampp.net" target="_blank"> 点击前往👆</a> </p>      <h3>国外节点服务器✨进阶</h3> <p>免费Clash订阅链接 <br> https://node.freeclashnode.com/uploads/2025/07/0-20250709.yaml <br> https://node.freeclashnode.com/uploads/2025/07/1-20250709.yaml <br> https://node.freeclashnode.com/uploads/2025/07/2-20250709.yaml <br> https://node.freeclashnode.com/uploads/2025/07/3-20250709.yaml <br> https://node.freeclashnode.com/uploads/2025/07/4-20250709.yaml </p> <br> <p>使用国外节点服务器可以达到全局使用的体验，国外付费节点更稳定高速<br>  这里我推荐飞鸟云，<h3>10元 / 200GB流量 | 不限速，不限设备数量 | 不限时，用完为止，套餐可叠加。</h3> <br>  <img src="https://file.uhsea.com/2507/f075430a8f7c7514e2d158672921ce867Q.jpg" alt="飞鸟云" width="750">  <br>  <img src="https://file.uhsea.com/2507/95223d9bba76244e80d81d87e86f141fXO.jpg" alt="飞鸟云" width="750">  <br>  购买链接: <a href="https://feiniaoyun.xyz/#/register?code=pl4OIsZ9" target="_blank"> 点击前往👆</a> <br>  </p><h3>国外节点服务器使用教程</h3> <h4>Clash Verge -rev 电脑端（支持Hysteria2协议）</h4> <br> <p> Clash Verge-rev使用教程  <br>  Windows7 需要安装框架  <br>  <a href="https://go.microsoft.com/fwlink/p/?LinkId=2124703" target="_blank"> 点击前往👆</a>  <br><br>  软件下载地址 <br> 64位  <a href="https://hollywood.lanzouj.com/win64dadwdqwd" target="_blank"> 点击前往👆</a> <br> 32位 <a href="https://hollywood.lanzouj.com/win32sdadwdaqdd" target="_blank"> 点击前往👆</a> <br><br> 64位备用下载地址 <br> https://cmhk.node-is.green/d/lh/Clash.Verge_2.2.3_x64_fixed_webview2-setup.exe <br> https://cmhk.node-is.green/d/lh/Clash.Verge_2.2.3_x64-setup.exe  <br> 第1步 <br>  <img src="https://s11.ax1x.com/2024/01/29/pFuw0ld.png" alt="飞鸟云" width="500"> <br> 然后打开安装的软件 <br> 第2步 点击👉配置👉将"订阅地址"粘贴到"输入框"👉点击"导入" <br>   <img src="https://picdm.sunbangyan.cn/2023/12/14/d39a6c969a21e6fce468ff0780de9ef6.jpeg" alt="飞鸟云" width="500"> <br> 第3步 设置👉关闭"IPV6"👉设置👉勾选"系统代理" <br>  <img src="https://picst.sunbangyan.cn/2023/12/14/a80363cd5f5f8128137808bc2e15f855.jpeg" alt="飞鸟云" width="500"> <br> 4.推荐选择规则模式，节点选择自动选择，节点不能选direct。 5.在桌面右下角，clash图标，右击，勾选系统代理。<br> 导入报错的话， 关闭Wi-Fi，用手机流量开热点给电脑导入或者先翻墙再导入。<br> 记得勾选系统代理，才能翻墙。<br> 💡【导入订阅到客户端报错】⭐️⭐️⭐️导入到客户端报错的话，浏览器打开,任意一个链接 <br> https://nexconvert.com/ | https://subcsub.com/ | https://sub-store.usuc.cc/ | https://subweb.datapipe.top/ | https://bianyuan.xyz/  <br> 填入订阅地址，其他不用改，往下拉，点击生成订阅地址，再复制转换后的订阅地址，填入到翻墙客户端，配置，粘贴URL即可。成功翻墙后，官网直接再导入一次订阅，不需要转换订阅。 ⭐️⭐️⭐️
+            若客户端下载不了，直接Bing必应搜索，客户端名字+下载。 </p>` },
+
+
+        '3': { title: '给大家推荐几款浏览器插件', content: `<h2>给大家推荐几款浏览器插件</h2><p>今天给大家推荐几个浏览器插件✨</p> <h3>第一款</h3> <p>AdGuard 广告拦截器</p> <br> <p>一款高效，免费的广告拦截工具</p> <img src="./img/3/1.jpg" alt="AdGuard" width="230"> <br> <p>安装链接：<a href="https://microsoftedge.microsoft.com/addons/detail/adguard-%E5%B9%BF%E5%91%8A%E6%8B%A6%E6%88%AA%E5%99%A8/pdffkfellgipmhklpdmokmckkkfcopbh" target="_blank"> 点击前往👆</a></p> <br> <h3>第二款</h3>  <p>IDM下载器(IDM Integration Module)</p> <br> <p>一款实用的多线程下载器</p> <br> <img src="./img/3/2.jpg" alt="IDM" width="500"> <p>官方下载链接: <a href="https://www.internetdownloadmanager.com/download.html" target="_blank"> 点击前往👆</a> <br> 注册机：<a href="https://wwnn.lanzouw.com/iywj9312g44b" target="_blank"> 点击前往👆</a> <p>插件安装链接：<a href="https://microsoftedge.microsoft.com/addons/detail/idm-integration-module/llbjbkhnmlidjebalopleeepgdfgcpec" target="_blank"> 点击前往👆</a> </p> <br> 首先正常安装好idm软件，默认简体中文语言👉成功安装后，先不要打开软件，将idm序列号注册机复制到软件目录下"默认目录"👉然后在目录下运行，点击Crack进行激活即可👉最后，打开软件，便可看到软件已经成功激活啦，可无限制免费使用所有功能 </p> <h3>第三款</h3> <p>百度药丸 Baidu Capsule</p> <br> <p>一款Baidu搜索引擎广告屏蔽优化插件</p> <img src="./img/3/3.jpg" alt="百度药丸 Baidu Capsule" width="200"> <br><br> <p>安装链接：<a href="https://microsoftedge.microsoft.com/addons/detail/baidu-adblocker-%E7%99%BE%E5%BA%A6%E8%8D%AF%E4%B8%B8/gpimdcibknljgdafpoopmpnpopbiahnp" target="_blank"> 点击前往👆</a> </p> <br> <img src="https://store-images.s-microsoft.com/image/apps.63477.9baa9855-b0c8-4e90-9acb-11e5c19a1e56.e797d55b-335b-4c20-ba6d-e2d71eb41db3.8d4d6050-d0d4-48d5-9ac2-b6cde77a3312" alt="百度药丸" width="500"> <img src="https://store-images.s-microsoft.com/image/apps.35712.9baa9855-b0c8-4e90-9acb-11e5c19a1e56.e797d55b-335b-4c20-ba6d-e2d71eb41db3.0e4ccdfc-d360-4d27-aaee-90f837bad49b" alt="百度药丸" width="500"> <br> <img src="https://store-images.s-microsoft.com/image/apps.65092.9baa9855-b0c8-4e90-9acb-11e5c19a1e56.e797d55b-335b-4c20-ba6d-e2d71eb41db3.672c80fb-420d-4054-a74e-6e266e7840b4" alt="百度药丸" width="500"> <img src="https://store-images.s-microsoft.com/image/apps.64219.9baa9855-b0c8-4e90-9acb-11e5c19a1e56.e797d55b-335b-4c20-ba6d-e2d71eb41db3.57c89ce5-52b1-4d35-9e55-06892972b6bd" alt="百度药丸" width="500"> <br> <h3>第四款</h3> <p>Listen 1</p> <br> <p>实用的免费多平台音乐播放器和搜索工具</p> <img src="./img/3/4.jpg" alt="Listen 1" width="800"> <p>安装链接：<a href="https://microsoftedge.microsoft.com/addons/detail/listen-1/bcneiehcbgahghfmgigmblcgkhihehad" target="_blank"> 点击前往👆</a> </p> <br> <h3>第五款</h3> <p>GithubCN</p> <br> <p>一款Github汉化插件</p> <img src="https://store-images.s-microsoft.com/image/apps.60431.2c3d45a8-95f5-475f-be22-4c1723c2968d.13ff62fa-26f4-4982-9118-afa2eab6184a.7ee7a77c-3ed6-4d81-943c-ab06bed96e8a" alt="GithubCN" width="500"> <img src="https://store-images.s-microsoft.com/image/apps.8911.2c3d45a8-95f5-475f-be22-4c1723c2968d.13ff62fa-26f4-4982-9118-afa2eab6184a.a5c4f67f-2f84-4140-a1e1-f47ca25e033c" alt="GithubCN" width="500"> <br> <img src="https://store-images.s-microsoft.com/image/apps.12131.2c3d45a8-95f5-475f-be22-4c1723c2968d.13ff62fa-26f4-4982-9118-afa2eab6184a.ef3005e9-4699-4923-8855-20ff3b66d676" alt="GithubCN" width="500"> <p>安装链接：<a href="https://microsoftedge.microsoft.com/addons/detail/githubcn/onlodfoebaobhmlhgcbddjngjbkdbfaj" target="_blank"> 点击前往👆</a> </p> <h3>第六款</h3> <p>沉浸式翻译</p> <br> <p>"沉浸式翻译"一款免费的（原文/译文）AI 双语对照网页翻译插件，同时支持PDF翻译（保留原文档排版），在线视频双语字幕翻译（Youtube, Netflix等），视频会议翻译（Google Meet, Zoom, Microsoft Teams），EPUB电子书翻译，图片翻译&漫画翻译；支持调用主流人工智能AI翻译大模型（OpenAI (ChatGPT)、DeepL、Deepseek、Gemini(Bard) 等 ）对以上内容进行更准确的翻译</p> <img src="https://store-images.s-microsoft.com/image/apps.40110.d49a3368-918f-446f-8d8b-4dadb0f08e8e.adea4d5e-dd32-42c7-9c2d-665a0fb9dfeb.33c49cd4-5f2d-470e-94fb-8aca00c1c39c" alt="沉浸式翻译" width="500"> <img src="https://store-images.s-microsoft.com/image/apps.34060.d49a3368-918f-446f-8d8b-4dadb0f08e8e.adea4d5e-dd32-42c7-9c2d-665a0fb9dfeb.4040c3f3-2044-4098-835e-023948768a6d" alt="沉浸式翻译" width="500"> <br> <img src="https://store-images.s-microsoft.com/image/apps.50256.d49a3368-918f-446f-8d8b-4dadb0f08e8e.adea4d5e-dd32-42c7-9c2d-665a0fb9dfeb.be251fe4-5e0a-4f07-9d5e-41ff823beaae" alt="沉浸式翻译" width="500"> <img src="https://store-images.s-microsoft.com/image/apps.34943.d49a3368-918f-446f-8d8b-4dadb0f08e8e.adea4d5e-dd32-42c7-9c2d-665a0fb9dfeb.7010f045-5e79-4c10-8c00-14303506b5ef" alt="沉浸式翻译" width="500"> <br> <img src="https://store-images.s-microsoft.com/image/apps.57280.d49a3368-918f-446f-8d8b-4dadb0f08e8e.adea4d5e-dd32-42c7-9c2d-665a0fb9dfeb.6119487e-3bfe-4e7d-a06f-5dfa98cc8c00" alt="沉浸式翻译" width="500"> <img src="https://store-images.s-microsoft.com/image/apps.25548.d49a3368-918f-446f-8d8b-4dadb0f08e8e.adea4d5e-dd32-42c7-9c2d-665a0fb9dfeb.66f8d9db-1ccf-4656-b458-0fd1e49c81c4" alt="沉浸式翻译" width="500"> <p>安装链接：<a href="https://microsoftedge.microsoft.com/addons/detail/%E6%B2%89%E6%B5%B8%E5%BC%8F%E7%BF%BB%E8%AF%91-%E7%BD%91%E9%A1%B5%E7%BF%BB%E8%AF%91%E6%8F%92%E4%BB%B6-pdf%E7%BF%BB%E8%AF%91-/amkbmndfnliijdhojkpoglbnaaahippg" target="_blank"> 点击前往👆</a> </p>`},
+
+    };
+
+    const siteFooterHTML = `<footer class="site-footer"><p>Copyright © 2025 科技酱 | 保留所有权利. KE JI JIANG | All rights reserved. <br>Copyright © 2025 电脑科学创始人 | 保留所有权利. Founder of Computer Science | All rights reserved.</p></footer>`;
+    const copyIconSVG = `<svg viewBox="0 0 24 24" fill="currentColor"><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"></path></svg>`;
+    const copiedIconSVG = `<svg viewBox="0 0 24 24" fill="currentColor"><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path></svg>`;
+    
+    // ... (所有其他JS代码保持不变，无需修改) ...
+    const searchInput = document.getElementById('search-input');
+    const noResultsMessage = document.getElementById('no-results-message');
+    const articleListEl = document.getElementById('article-list');
+    const contentAreaEl = document.getElementById('article-content-area');
+    const backToTopBtn = document.getElementById('back-to-top-btn');
+    const tocSidebarEl = document.getElementById('toc-sidebar');
+    const tocHeaderEl = document.getElementById('toc-header');
+    const tocListWrapperEl = document.getElementById('toc-list-wrapper');
+    const tocListEl = document.getElementById('toc-list');
+    const tocIndicatorEl = document.getElementById('toc-indicator');
+    let headings = [];
+    let articleLiElements = [];
+    function handleHashChange() { let articleId = window.location.hash.substring(1); if (!articles[articleId]) { articleId = Object.keys(articles)[0]; window.history.replaceState(null, null, `#${articleId}`); } loadArticle(articleId); updateActiveArticleLink(articleId); }
+    function updateActiveArticleLink(articleId) { articleListEl.querySelectorAll('a').forEach(link => { link.classList.remove('active'); if (link.getAttribute('href') === `#${articleId}`) { link.classList.add('active'); } }); }
+    searchInput.addEventListener('input', (e) => { const searchTerm = e.target.value.toLowerCase().trim(); let visibleCount = 0; articleLiElements.forEach(li => { const title = li.querySelector('a').textContent.toLowerCase(); const shouldShow = title.includes(searchTerm); li.style.display = shouldShow ? 'block' : 'none'; if (shouldShow) visibleCount++; }); noResultsMessage.style.display = visibleCount === 0 ? 'block' : 'none'; });
+    contentAreaEl.addEventListener('scroll', () => {  updateTocHighlight(); if (contentAreaEl.scrollTop > 300) { backToTopBtn.classList.add('visible'); }  else { backToTopBtn.classList.remove('visible'); }  });
+    backToTopBtn.addEventListener('click', () => { contentAreaEl.scrollTo({ top: 0, behavior: 'smooth' }); });
+    tocHeaderEl.addEventListener('click', () => {  tocSidebarEl.classList.toggle('collapsed');  if (tocSidebarEl.classList.contains('collapsed')) { tocListWrapperEl.style.maxHeight = '0px'; }  else { tocListWrapperEl.style.maxHeight = tocListEl.scrollHeight + "px"; }  });
+    document.body.addEventListener('click', e => {  const button = e.target.closest('.copy-button');  if (button) {  const code = button.closest('.code-block-wrapper').querySelector('code').innerText;  navigator.clipboard.writeText(code).then(() => {  button.innerHTML = copiedIconSVG; button.classList.add('copied');  setTimeout(() => { button.innerHTML = copyIconSVG; button.classList.remove('copied'); }, 2000);  });  }  });
+    tocListEl.addEventListener('click', (e) => { const link = e.target.closest('a'); if (link) { e.preventDefault(); const targetId = link.getAttribute('href').substring(1); const targetElement = document.getElementById(targetId); if (targetElement) { contentAreaEl.scrollTo({ top: targetElement.offsetTop - 20, behavior: 'smooth' }); } } });
+    function generateToc() { tocListEl.querySelectorAll('li').forEach(li => li.remove()); headings = Array.from(contentAreaEl.querySelectorAll('h2, h3')); if (headings.length === 0) { tocSidebarEl.classList.add('hidden'); return; } tocSidebarEl.classList.remove('hidden'); headings.forEach((h, i) => { h.id = `h-${i}`; const li = document.createElement('li'); const a = document.createElement('a'); a.href = `#${h.id}`; a.textContent = h.textContent; if (h.tagName === 'H2') a.classList.add('toc-level-2'); if (h.tagName === 'H3') a.classList.add('toc-level-3'); li.appendChild(a); tocListEl.appendChild(li); }); if (!tocSidebarEl.classList.contains('collapsed')) { tocListWrapperEl.style.maxHeight = tocListEl.scrollHeight + "px"; } }
+    function updateTocHighlight() { if (headings.length === 0) return; let activeLink = null; const scrollOffset = contentAreaEl.scrollTop + 80; const links = tocListEl.querySelectorAll('a'); links.forEach(l => l.classList.remove('active-toc-link')); for (let i = headings.length - 1; i >= 0; i--) { if (headings[i].offsetTop <= scrollOffset) { activeLink = links[i]; break; } } if (activeLink) { activeLink.classList.add('active-toc-link'); const parentLi = activeLink.parentElement; tocIndicatorEl.style.top = `${parentLi.offsetTop}px`; tocIndicatorEl.style.height = `${parentLi.offsetHeight}px`; tocIndicatorEl.style.opacity = '1'; } else { tocIndicatorEl.style.opacity = '0'; } }
+    function enhanceCodeBlocks() { document.querySelectorAll('.article-body pre').forEach(pre => { if (pre.parentElement.classList.contains('code-block-wrapper')) return; const code = pre.querySelector('code'); if (!code) return; const wrapper = document.createElement('div'); wrapper.className = 'code-block-wrapper'; const header = document.createElement('div'); header.className = 'code-block-header'; const copyButton = document.createElement('button'); copyButton.className = 'copy-button'; copyButton.innerHTML = copyIconSVG; copyButton.title = "复制到剪贴板"; header.appendChild(copyButton); pre.parentNode.insertBefore(wrapper, pre); wrapper.appendChild(header); wrapper.appendChild(pre); }); }
+    function loadArticle(articleId) {  const article = articles[articleId] || { content: `<h2>文章未找到</h2>` };  contentAreaEl.innerHTML = `<div class="article-body">${article.content}</div>` + siteFooterHTML;  contentAreaEl.scrollTop = 0;  enhanceCodeBlocks();  generateToc();  updateTocHighlight();  }
+    function initialize() {  Object.keys(articles).forEach((key) => {  const a = document.createElement('a');  a.href = `#${key}`; a.dataset.article = key;  a.textContent = articles[key].title;  const li = document.createElement('li');  li.appendChild(a);  articleListEl.appendChild(li);  articleLiElements.push(li); });  window.addEventListener('hashchange', handleHashChange); handleHashChange(); }
+    initialize();
+});
